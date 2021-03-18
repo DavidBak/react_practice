a731925607b4c84b0fc55f94ec87c89d8aacd3ad
DavidBak <sejongbgy07@gmail.com>
2021-03-18 15:41:57 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require("./models/User.js")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://David:aktwls7179@boilerplate.c7fms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
@@ -11,7 +20,19 @@ mongoose.connect('mongodb+srv://David:aktwls7179@boilerplate.c7fms.mongodb.net/m
 
 
 app.get('/', (req, res) => {
-  res.send('안녕하세요')
+  res.send('hello')
+})
+
+app.post('/register',(req,res)=>{
+  
+  const user = new User(req.body)
+
+  user.save((err, userInfo)=>{
+    if(err){return res.json({success: false, err})}
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
