66980b6f9e49cc3ab7c34447d5b88b2a9979e152
DavidBak <sejongbgy07@gmail.com>
2021-03-22 01:05:25 +0900

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,17 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage.js'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import Register from './components/views/RegisterPage/RegisterPage'
+import Auth from './hoc/auth'
+
 
 function App() {
   return (
     <Router>
       <div> 
         <Switch>
-          <Route exact path="/" component={LandingPage}/>
-          <Route exact path="/login" component={LoginPage}/>
-          <Route exact path="/register" component={Register}/>
+          <Route exact path="/" component={Auth(LandingPage, null)}/>
+          <Route exact path="/login" component={Auth(LoginPage, false)}/>
+          <Route exact path="/register" component={Auth(Register, false)}/>
         </Switch>
       </div>
     </Router>
